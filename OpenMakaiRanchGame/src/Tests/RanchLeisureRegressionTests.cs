98eafb93955f3934b4bd1d228366089c5c922092
Paul Schindler-Saefkow <noa3@gmx.net>
2026-09-10 21:53:10 +0200

    test: import the existing eligibility enum for the synthetic companion fixture

--- OpenMakaiRanchGame/src/Tests/RanchLeisureRegressionTests.cs
+++ OpenMakaiRanchGame/src/Tests/RanchLeisureRegressionTests.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Text.Json;
 using OpenMakaiRanch.App;
 using OpenMakaiRanch.Core.Models;
+using OpenMakaiRanch.Core.Resources;
 using OpenMakaiRanch.Gameplay;
 
 namespace OpenMakaiRanch.Tests;
